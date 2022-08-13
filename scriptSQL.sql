4ff5de623ac6547326b7b7de386c2ad4fb98e93c
DiegolPl <diegopdl1992@gmail.com>
2022-08-13 12:04:37 -0300

    Agrego el resto de los ejercicios de SQL

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -112,5 +112,54 @@ INSERT INTO Dueno VALUES (36225649,'Javier','Medina','3514744635','Pte Peron 442
 INSERT INTO Perro(Nombre, Fecha_nac, Sexo, DNI_Dueno) VALUES ('Snoopy', '2022-03-03', 'Macho', 36225649);
 INSERT INTO Historial(Fecha, Perro_ID, Descripcion, Monto) VALUES ('2022-08-11',13,'Corte',1100);
 
+-- 3. Borre un animal que ya no va a ser atendido. Para ello consulte antes en el historial, algún animal que ya no sea atendido 
+-- desde hace mucho tiempo.
+
+-- Consulto la ultima vez que fueron atendidos 
+SELECT MAX(Fecha) as Fecha, Perro_ID FROM Historial
+GROUP BY Perro_ID
+ORDER BY Fecha Asc;
+
+-- Para el ejercicio, borro el perro que más tiempo lleva sin venir (Perro id 2)
+DELETE FROM Historial WHERE Perro_ID = 2; -- Borro historial
+DELETE FROM Perro WHERE ID_Perro = 2; -- Borro animal
+
+-- 4. Actualice la fecha de nacimiento de algún animal (perro) que usted considere. - Snoopy (ID: 13)
+UPDATE Perro
+SET Fecha_nac = '2022-03-01'
+WHERE ID_Perro = 13;
+
+-- 5. Realice una consulta multitabla que arroje el nombre de todos los perros cuyos dueños se llaman Pedro
+SELECT p.Nombre as Nombre
+FROM Perro p LEFT JOIN Dueno d ON d.DNI = p.DNI_Dueno
+WHERE d.Nombre = 'Pedro';
+
+-- 6. Obtener todos los perros que asistieron a la peluquería en 2022
+SELECT DISTINCT p.Nombre as Nombre
+FROM Perro p LEFT JOIN Historial h ON h.Perro_ID = p.ID_Perro
+WHERE h.Fecha > '2022-01-01';
+
+-- 7. Obtener los ingresos percibidos en Julio del 2022
+SELECT SUM(Monto) FROM Historial
+WHERE MONTH(Fecha) = 07 AND YEAR(Fecha) = 2022;
+
 -- 8. Insertar un nuevo registro en la tabla historial de un perro cuyo ID Perro es igual a 10.
 INSERT INTO Historial (Fecha, Perro_ID, Descripcion, Monto) VALUES ('2022-08-11',10,'Corte',1100);
+
+-- 9. Escriba una consulta que permita actualizar la dirección de un dueño. Su nueva dirección es Libertad 123
+UPDATE Dueno 
+SET Direccion = 'Libertad 123'
+WHERE DNI = 29261577;
+
+-- 10. Vaciar la tabla historial y resetear el contador del campo ID.
+TRUNCATE TABLE Historial;
+ALTER TABLE Historial AUTO_INCREMENT = 1;
+
+-- 11. Obtener todos los dueños que tengan perros de menos de 5 años de edad que no hayan visitado la peluquería en el año 2022.
+SELECT DISTINCT d.*
+FROM Dueno d LEFT JOIN Perro p ON d.DNI = p.DNI_Dueno LEFT JOIN Historial h ON p.ID_Perro = h.Perro_ID
+WHERE TIMESTAMPDIFF(YEAR,p.Fecha_nac,CURRENT_TIME()) < 5 AND h.Fecha NOT LIKE '2022%';
+
+-- 12. Obtener todos los perros de sexo “Macho” nacidos entre 2020 y 2022.
+SELECT * FROM Perro
+WHERE Sexo = 'Macho' AND YEAR(Fecha_nac) IN (2020,2021,2022);
